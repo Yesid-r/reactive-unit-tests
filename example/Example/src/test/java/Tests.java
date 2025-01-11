4545d5dbee4bd8a96512a8ff6eeb2a4e82165a89
Yesid Rincon <dairo.rincon@uptc.edu.co>
2025-01-11 18:46:23 -0500

    add tests

--- example/Example/src/test/java/Tests.java
+++ example/Example/src/test/java/Tests.java
@@ -5,31 +5,48 @@ public class Tests {
 
     @Test
     void showMessageMethodShouldReturnOneValue() {
+        StepVerifier.create(Examples.showMessage())
+                .expectNextCount(1)
+                .verifyComplete();
 
     }
 
     @Test
     void showMessageMethodShouldStartWithLetterE() {
 
+        StepVerifier.create(Examples.showMessage())
+                .expectNextMatches(message -> message.startsWith("E"))
+                .verifyComplete();
     }
 
     @Test
     void customersNameByIdShouldReturnCarlosIfIdIs2() {
-
+        StepVerifier.create(Examples.customersNameById("2"))
+                .expectNext("Carlos")
+                .verifyComplete();
     }
 
     @Test
     void customersNameByIdShouldReturnEmptyIfIdIsAbove3() {
-
+        StepVerifier.create(Examples.customersNameById("4"))
+                .verifyComplete();
     }
 
     @Test
     void upperCaseMessageShouldReturnError() {
+        StepVerifier.create(Examples.upperCaseMessage())
+                .expectNext("NO")
+                .expectError()
+                .verify();
 
     }
 
     @Test
     void upperCaseMessageShouldReturnNullPointerException() {
 
+        StepVerifier.create(Examples.upperCaseMessage())
+                .expectNext("NO")
+                .expectError(NullPointerException.class)
+                .verify();
     }
 }
